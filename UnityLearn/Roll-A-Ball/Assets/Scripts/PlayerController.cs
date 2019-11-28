1dc997806e2686180da26c5b884bceaf8bab3c9c
RedbeardDoesDev <redbeard.jonathan@gmail.com>
2019-11-28 00:29:30 -0600

    Add PlayerController script for moving player object

---
+++ UnityLearn/Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+
+    public float Speed;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        _rigidBody.AddForce(new Vector3(moveHorizontal, 0f, moveVertical) * Speed);
+    }
+}
