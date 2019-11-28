cb076756851cce791832abf06b776e7917f728e0
RedbeardDoesDev <redbeard.jonathan@gmail.com>
2019-11-28 09:08:29 -0600

    Show score and add win condition

--- UnityLearn/Roll-A-Ball/Assets/Scripts/PlayerController.cs
+++ UnityLearn/Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rigidBody;
+    private int _count;
 
     public float Speed;
+    public Text CountText, WinText;
 
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+
+        _count = 0;
+        SetCount();
+
+        WinText.text = "YOU WIN!!!!!!";
+        WinText.enabled = false;
     }
 
     void FixedUpdate()
@@ -23,7 +33,19 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
+            _count++;
             other.gameObject.SetActive(false);
+            SetCount();
+        }
+    }
+
+    void SetCount()
+    {
+        CountText.text = "Count: " + _count;
+
+        if (_count == 10)
+        {
+            WinText.enabled = true;
         }
     }
 }
