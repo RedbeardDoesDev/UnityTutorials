7b84dcba9978a861e520e2707e6721fd92dc9306
RedbeardDoesDev <redbeard.jonathan@gmail.com>
2019-11-28 00:33:37 -0600

    Camera follows player object

---
+++ UnityLearn/Roll-A-Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject PlayerObject;
+
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - PlayerObject.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = PlayerObject.transform.position + _offset;
+    }
+}
